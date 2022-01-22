2bda5626faef845565e75bda1107c526f340a014
Captain63 <stephen.a.rodd@gmail.com>
2022-01-22 13:14:38 -0500

    Build basic architecture for RPS

--- src/App.js
+++ src/App.js
@@ -5,17 +5,83 @@ const App = () => {
   const [rpsState, setRPSState] = useState({ 
     humanChoice: null,
     computerChoice: null,
-    countdown: 3
+    countdown: 3,
+    round: 1,
+    computerTally: 0,
+    humanTally: 0,
+    tieTally: 0,
+    currentWinner: null
   });
 
-  const humanChoices = ["Rock", "Paper", "Scissors"];
+  const choices = ["Rock", "Paper", "Scissors"];
+
+  const selectWinner = () => {
+    console.log(rpsState.humanChoice);
+
+    if (rpsState.humanChoice === rpsState.computerChoice) {
+      const tieCount = ++rpsState.tieTally;
+
+      setRPSState(prevState => {
+        return {
+          ...prevState,
+          tieTally: tieCount,
+          currentWinner: "Draw!"
+        }
+      })
+
+      // Stop checking other conditions
+      return;
+    }
+
+    if (
+      (rpsState.humanChoice === "rock" && rpsState.computerChoice === "scissors") ||
+      (rpsState.humanChoice === "paper" && rpsState.computerChoice === "rock") ||
+      (rpsState.humanChoice === "scissors" && rpsState.computerChoice === "paper")
+    ) {
+      const humanTally = ++rpsState.humanTally;
+
+      setRPSState(prevState => {
+        return {
+          ...prevState,
+          humanTally: humanTally,
+          currentWinner: "You!"
+        }
+      })
+    } else {
+      const compTally = ++rpsState.computerTally;
+
+      setRPSState(prevState => {
+        return {
+          ...prevState,
+          computerTally: compTally,
+          currentWinner: "The Machine"
+        }
+      })
+    }
+  }
+
+  const computerChooses = () => {
+    const choice = choices[Math.floor(Math.random() * choices.length)].toLowerCase();
+
+    setRPSState(prevState => {
+      return {
+        ...prevState,
+        computerChoice: choice
+      }
+    })
+
+    countdown();
+  }
 
   const countdown = () => {
     const countdownInterval = setInterval(() => 
     {
-      if (rpsState.countdown === 0) clearInterval(countdownInterval);
+      const count = --rpsState.countdown;
 
-      const count = rpsState.countdown--;
+      if (rpsState.countdown === 0) {
+        clearInterval(countdownInterval)
+        selectWinner();
+      };
 
       setRPSState(prevState => {
         return {
@@ -34,28 +100,47 @@ const App = () => {
       }
     });
 
-    countdown();
+    computerChooses();
+  }
+
+  const playAgain = () => {
+    const newRoundCount = ++rpsState.round;
+
+    setRPSState(prevState => {
+      return {
+        ...prevState,
+        humanChoice: null,
+        computerChoice: null,
+        countdown: 3,
+        round: newRoundCount
+      }
+    });
   }
 
   const reset = () => {
     setRPSState({
       humanChoice: null,
       computerChoice: null,
-      countdown: 3
+      countdown: 3,
+      round: 1,
+      computerTally: 0,
+      humanTally: 0,
+      tieTally: 0,
+      currentWinner: null
     });
   }
 
   return (
     <div className="App">
       <header>
-        <h1>The Game of Champions</h1>
+        <h1>Rock, Paper, Scissors: The Game of Champions</h1>
       </header>
       <main>
         <section>
           <h2>Make Your Choice</h2>
           <div>
             {
-              humanChoices.map((value, i) => (<button key={value.toLowerCase()} onClick={choose} disabled={rpsState.humanChoice}>{value}</button>))
+              choices.map((value, i) => (<button key={value.toLowerCase()} onClick={choose} disabled={rpsState.humanChoice}>{value}</button>))
             }
           </div>
         </section>
@@ -64,7 +149,11 @@ const App = () => {
             {
               rpsState.countdown ? 
               <h2>Computer Choice in {rpsState.countdown}...</h2> :
-              <h2>Computer Chooses:</h2>
+              <>
+                <h2>Computer Chooses: {rpsState.computerChoice.charAt(0).toUpperCase() + rpsState.computerChoice.slice(1)}</h2>
+                <h2>Round {rpsState.round} Winner: {rpsState.currentWinner}</h2>
+                <button onClick={playAgain}>Play Again</button>
+              </>
             }
             <button onClick={reset}>Reset</button>
           </section>
